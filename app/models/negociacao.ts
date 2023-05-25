abd1f7b7c6fd83a62437c973eba49d86b23d7a96
Daniel Alex <dalexperia@gmail.com>
2023-05-25 10:30:25 -0300

    Substituicao dos getters no model negociao por public readonly

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,21 +1,9 @@
 export class Negociacao {
 
     constructor(
-        private _data: Date, 
-        private _quantidade: number, 
-        private _valor: number) {}
-
-    get data(): Date{
-        return this._data;
-    }
-
-    get quantidade(): number {
-        return this._quantidade;
-    }
-
-    get valor(): number {
-        return this._valor;
-    }
+        public readonly data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number) {}
 
     get volume(): number {
         return this.valor * this.quantidade
