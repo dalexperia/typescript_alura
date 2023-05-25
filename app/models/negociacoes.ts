4b6d8355fb1f47d293c2f5620bf9b5e2823afecd
Daniel Alex <dalexperia@gmail.com>
2023-05-25 08:59:14 -0300

    Iniciando conceito de generics em TS e criacao do metodo adiciona no model negociacoes

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -3,5 +3,8 @@ import { Negociacao } from "./negociacao.js";
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
 
+    adiciona(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
 
 }
