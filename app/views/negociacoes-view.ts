9dd97806c50aea9ab46b6ae04d688247dd0d9947
Daniel Alex <dalexperia@gmail.com>
2023-05-25 11:01:55 -0300

    Criacao da view negociacoesView que retorna um template: tabela

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,17 @@
+export class negociacoesView {
+    template(): string {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                <th>DATA</th>
+                <th>QUANTIDADE</th>
+                <th>VALOR</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+        `;
+    }
+}
