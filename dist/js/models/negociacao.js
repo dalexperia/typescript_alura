47078f20260a37111ffc0551810e3bc91b8d3959
Daniel Alex <dalexperia@gmail.com>
2023-05-25 10:46:44 -0300

    Para evitar que a data seja alterada, crie um getter para a propriedade data e retorne uma copia

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,10 +1,15 @@
 export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this.data = data;
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
     }
     get volume() {
         return this.valor * this.quantidade;
+    }
+    get data() {
+        // Programação defensiva: crie um copia da data original e a retorne
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
