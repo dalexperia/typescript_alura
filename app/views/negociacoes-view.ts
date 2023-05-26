5a28aea11f94d44cfe02bb8e5f2d2a78420cf252
Daniel Alex <dalexperia@gmail.com>
2023-05-26 10:38:40 -0300

    Removido propriedade e constructor de negociacoes-view

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -3,12 +3,6 @@ import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacoesView {
 
-    private elemento: HTMLElement;
-
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor);
-    }
-
     template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
