56f80ba7d5919b968b96b5d2c882a3e708e36a13
Daniel Alex <dalexperia@gmail.com>
2023-05-26 19:54:45 -0300

    Tratando valores null em app.ts

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,11 @@ import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector(".form");
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    controller.adiciona();
-})
+if(form){
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        controller.adiciona();
+    })
+}else{
+    throw Error('Form não existe, favor verificar');
+}
