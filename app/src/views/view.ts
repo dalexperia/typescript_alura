006bce1f3de358a585ca67cde7e6646b62a2b326
Daniel Alex <dalexperia@gmail.com>
2023-05-26 20:38:29 -0300

    Calculo de performance do arquivo view.ts

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -18,10 +18,16 @@ export abstract class View<T> {
     protected abstract template(model: T): string;
 
     public update(model: T): void {
+        /*
+        Calcular a performance da página
+        */
+        const t1 = performance.now();
         let template = this.template(model);
         if(this.scape) {
             template = template.replace(/<script>[\s\S]*?<\/script>/,'');
         }
         this.elemento.innerHTML = template;
+        const t2 = performance.now();
+        console.log(`Tempo de execução do método update: ${(t2 - t1)/1000} segundos`)
     }
 }
