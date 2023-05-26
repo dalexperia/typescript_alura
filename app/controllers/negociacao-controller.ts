9f4de8fc6f6b6edaa6c54353df2376f4a1696752
Daniel Alex <dalexperia@gmail.com>
2023-05-26 16:36:04 -0300

    Insere enum diadasemana em negociacao-controller

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiaDaSemana } from "../enums/diasDeSemana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -10,8 +11,6 @@ export class NegociacaoController {
     private negociacoes: Negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView("#negociacoesView");
     private mensagemView = new MensagemView("#mensagemView");
-    private readonly SABADO = 6;
-    private readonly DOMINGO = 0;
 
     constructor() {
         this.inputData = document.querySelector("#data");
@@ -32,7 +31,7 @@ export class NegociacaoController {
     }
 
     private diaUtil(data: Date): boolean {
-        return data.getDay() > this.DOMINGO && data.getDay() < this.SABADO
+        return data.getDay() > DiaDaSemana.DOMINGO && data.getDay() < DiaDaSemana.SABADO
     }
 
     private criaNegociacao(): Negociacao {
