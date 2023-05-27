5812901c5fa084ab6f80e806aee5a6d55d9cdf6c
Daniel Alex <dalexperia@gmail.com>
2023-05-26 22:03:09 -0300

    Codificando a funcao decorator logaTempoExceucao

--- app/src/decorators/logarTempoExecucao.ts
+++ app/src/decorators/logarTempoExecucao.ts
@@ -4,6 +4,14 @@ export function logarTempoExecucao() {
         propertyKey: string,
         descriptor: PropertyDescriptor
     ){
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function(...args: Array<any>) {
+            const t1 = performance.now();
+            const retorno = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+            console.log(`Tempo de execução do método ${propertyKey}: ${(t2 - t1)/1000} segundos`);
+            retorno
+        }
         return descriptor;
     }
 }
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempoExecucao } from "../decorators/logarTempoExecucao.js";
+
 export abstract class View<T> {
     protected elemento: HTMLElement;
     private scape = false;
@@ -17,17 +19,13 @@ export abstract class View<T> {
 
     protected abstract template(model: T): string;
 
+    @logarTempoExecucao()
     public update(model: T): void {
-        /*
-        Calcular a performance da página
-        */
-        const t1 = performance.now();
         let template = this.template(model);
         if(this.scape) {
             template = template.replace(/<script>[\s\S]*?<\/script>/,'');
         }
         this.elemento.innerHTML = template;
-        const t2 = performance.now();
-        console.log(`Tempo de execução do método update: ${(t2 - t1)/1000} segundos`)
+
     }
 }
